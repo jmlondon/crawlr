b41ae5cd7861e03f098046636865df05ab8cdabc
Josh London <josh.london@noaa.gov>
2016-08-11 15:56:15 -0700

    ADD SUPPORT FOR FILTERING BY locType

--- R/get_sim_points.R
+++ R/get_sim_points.R
@@ -1,18 +1,19 @@
 #' SpatialPoints from a list of Simulated Tracks
 #'
 #' @param sim_tracks
+#' @param locType
 #'
 #' @return a SpatialPointsDataFrame containing all points from sim_tracks
 #' @export
 #'
-get_sim_points <- function(sim_tracks) {
+get_sim_points <- function(sim_tracks, locType = c('p','o')) {
   simPoints = lapply(sim_tracks, function(x) data.frame(mu.x = x$alpha.sim[,'mu.x'],
                                                         mu.y = x$alpha.sim[,'mu.y'],
                                                         num_time = x$Time,
                                                         locType = x$locType
   ))
 
-  simPoints <- dplyr::bind_rows(simPoints)
+  simPoints <- dplyr::bind_rows(simPoints) %>% filter(locType %in% locType)
 
   sp::coordinates(simPoints) <- ~mu.x+mu.y
   sp::proj4string(simPoints) <- sp::CRS("+init=epsg:3571")
