cc905f9090acf5ff931186e106c2f7d36e184ad9
Josh London <josh.london@noaa.gov>
2016-12-09 19:58:31 -0800

    REMOVE seed PARAM & ALLOW FOR study_area
    this now allows the user to provide a study_area instead of basing the grid off the extent of the data_sp object

--- R/ud_hexpolys.R
+++ R/ud_hexpolys.R
@@ -3,7 +3,6 @@
 #' @param data_sp SpatialPointsDataFrame of regular locations
 #' @param cellsize cellsize parameter passed to spsample
 #' @param leaflet should the output be converted to EPSG:4326 for leaflet
-#' @param seed for consistency
 #' @param cellsize.override whether to override cellsize specified value would result in poor performance
 #'
 #' @return a SpatialPolygonsDataFrame
@@ -39,30 +38,47 @@
 #     fillColor = ~ pal(log(count)),
 #     weight = 1
 #   )
-ud_hexpolys <- function(data_sp, cellsize, leaflet = FALSE,
-                        density = TRUE, seed = 123,
+ud_hexpolys <- function(data_sp, study_area, cellsize, leaflet = FALSE,
+                        density = TRUE,
                         cellsize.override = TRUE) {
-  bb <- bbox(data_sp)
-  max_dim <- max(bb["mu.x","max"] - bb["mu.x","min"],
-                 bb["mu.y","max"] - bb["mu.y","min"])
 
-  if (max_dim/cellsize > 150) {
-    if (cellsize.override) {
-    cellsize = 1250
-    warning(
-      paste0("cellsize of", cellsize, "will result in poor",
-             "performance. setting to 1250 meters. if you want",
-             "a higher resolution, specify cellsize.override = FALSE.")
-    )
+  if (missing(study_area)) {
+    bb <- bbox(data_sp)
+    max_dim <- max(bb["mu.x","max"] - bb["mu.x","min"],
+                   bb["mu.y","max"] - bb["mu.y","min"])
+
+    if (max_dim/cellsize > 150) {
+      if (cellsize.override) {
+        cellsize = 1250
+        warning (
+          paste("cellsize of", cellsize, "will result in poor",
+                 "performance. setting to 1250 meters. if you want",
+                 "a higher resolution, specify cellsize.override = FALSE."
+                )
+        )
+      }
     }
+
+    set.seed(seed)
+
+    hex_polys <- sp::spsample(data_sp,
+                              cellsize = cellsize,
+                              type = "hexagonal",
+                              offset = c(0, 0))
+    hex_polys <- sp::HexPoints2SpatialPolygons(hex_polys)
   }
 
-  set.seed(seed)
+  if (!missing(study_area)) {
+    if (is.na(proj4string(study_area))) {
+      stop("proj4string for study_area is NA")
+    }
 
-  hex_polys <- sp::spsample(data_sp,
-                        cellsize = cellsize,
-                        type = "hexagonal")
-  hex_polys <- sp::HexPoints2SpatialPolygons(hex_polys)
+    hex_polys <- sp::spsample(study_area,
+                              cellsize = cellsize,
+                              type = "hexagonal",
+                              offset = c(0, 0))
+    hex_polys <- sp::HexPoints2SpatialPolygons(hex_polys)
+  }
 
   hex_over <- over(data_sp,hex_polys)
 
