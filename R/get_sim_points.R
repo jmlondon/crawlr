08afddc3093cd8c0296bcdff2173cdcadda508ac
Josh London <josh.london@noaa.gov>
2017-01-23 11:14:06 -0800

    ADD OPTION TO SPECIFY CRS

--- R/get_sim_points.R
+++ R/get_sim_points.R
@@ -2,11 +2,12 @@
 #'
 #' @param sim_tracks simulated tracks returned from get_sim_tracks()
 #' @param locType which locTypes to filter
+#' @param crs a valid CRS object that will be associated with returned SPDF
 #'
 #' @return a SpatialPointsDataFrame containing all points from sim_tracks
 #' @export
 #'
-get_sim_points <- function(sim_tracks, locType = c('p','o')) {
+get_sim_points <- function(sim_tracks, locType = c('p','o'), crs) {
 
   sim_tracks <- lapply(sim_tracks,
                       function(x) {
@@ -22,5 +23,8 @@ get_sim_points <- function(sim_tracks, locType = c('p','o')) {
     dplyr::filter(locType %in% locType)
 
   sp::coordinates(simPoints) <- ~mu.x+mu.y
+  if (!missing(crs)) {
+    sp::proj4string(simPoints <- crs)
+  }
   return(simPoints)
 }
